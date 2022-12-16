3e872a0a5c676d014d3a751353c2005b4e264bd6
Valeria Zavadetska <Valeria.zavadetska@gmail.com>
2022-12-16 11:14:29 +0200

    create hw 19

---
+++ js/app.js
@@ -0,0 +1,55 @@
+let arr = ["0", "1", "2", "3", "4"]; 
+let count = 0; 
+let timer; 
+ 
+const buttonForward = document.querySelector(".forward"); 
+buttonForward.addEventListener("click", changePhotoForward); 
+ 
+const buttonBack = document.querySelector(".back"); 
+buttonBack.addEventListener("click", changePhotoBack); 
+ 
+function setImage(indexImage) { 
+  const img = document.querySelector(".photo"); 
+  img.src = "./img/" + arr[indexImage] + ".jpg"; 
+} 
+ 
+function showSlides() { 
+  console.log(count); 
+  
+  count ++ ; 
+
+  if (count === arr.length) { 
+    count = 0; 
+  }
+
+  setImage(count); 
+ 
+  timer = setTimeout(showSlides, 3000); 
+} 
+ 
+function changePhotoForward() { 
+  clearInterval(timer); 
+    count ++; 
+
+    if (count === arr.length) { 
+        count = 0; 
+      } 
+
+  setImage(count); 
+ 
+  timer = setTimeout(showSlides, 3000); 
+} 
+ 
+function changePhotoBack() { 
+  clearInterval(timer);
+  count --; 
+
+  if (count === -1) { 
+    count = arr.length - 1; 
+  } 
+
+  setImage(count); 
+  timer = setTimeout(showSlides, 3000); 
+} 
+ 
+showSlides();
